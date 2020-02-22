8d7518824c87385c9fc93efa055cc7c2dde3f01a
Lexi Burroughs <lexi.burroughs.skate.823@gmail.com>
2020-02-22 15:19:55 -0500

    Initial Commit

---
+++ OutfitDesigner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OutfitDesigner
+//
+//  Created by Lexi Burroughs on 2/22/20.
+//  Copyright © 2020 Lexi Burroughs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
